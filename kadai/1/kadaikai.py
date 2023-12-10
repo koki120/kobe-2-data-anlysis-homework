e73ce63a14ca3de760b5df7084cec99387b814e0
nakstsu <koki20001206@outllok.jp>
2023-12-10 15:35:53 +0000

    add 主成分分析

--- kadai/1/kadaikai.py
+++ kadai/1/kadaikai.py
@@ -1,16 +1,7 @@
+from math import sqrt
 import pandas as pd
-import numpy as np
 from sklearn import linear_model
-
-# 引数ddof=1とするとn-1で割る標準偏差が使われる. デフォルトはaxis=Noneで配列全体
-
-
-def standardization(x: np.ndarray, axis=None, ddof=1) -> np.ndarray:
-    x_mean = x.mean(axis=axis, keepdims=True)
-    x_std = x.std(axis=axis, keepdims=True, ddof=ddof)
-    result = (x - x_mean) / x_std
-    return result
-
+from sklearn.decomposition import PCA
 
 economicData = pd.read_csv(
     '1/県内総生産.csv', sep=',')
@@ -19,6 +10,9 @@ municipalityAndAreaData = pd.read_csv(
 populationData = pd.read_csv(
     '1/年齢区分別人口.csv', sep=',')
 merged_data = pd.merge(municipalityAndAreaData, populationData, on='都道府県')
+
+
+# 回帰分析
 
 # 回帰モデルの呼び出し
 clf = linear_model.LinearRegression()
@@ -35,15 +29,45 @@ y = economicData['名目県内総生産'].values
 # 予測モデルを作成（重回帰）
 clf.fit(x, y)
 
-# 回帰係数と切片の抽出
+
+# 結果を表示
 a = clf.coef_
 b = clf.intercept_
 r_squared = clf.score(x, y)
 n = x.shape[0]
 k = x.shape[1]
 adjusted_r_squared = r_squared*(n-1)/(n-1-k) - k/(n-1-k)
-# 回帰係数
 print("回帰係数:", a)
 print("切片:", b)
+# 定義が[残差変動の平方和/((全変動の平方和*回帰変動の平方和)の2乗根)]なので負にならない
+print("重相関係数:", sqrt(r_squared))
 print("決定係数:", r_squared)
 print("自由度調整済み決定係数:", adjusted_r_squared)
+
+
+# 主成分分析
+
+all_merged_data = pd.merge(economicData, merged_data, on='都道府県')
+df = all_merged_data[['都道府県', '名目県内総生産', '市', '町',
+                      '村', '総面積', '0歳から14歳', '15歳から64歳', '65歳以上']]
+# 標準化
+dfs = df.iloc[:, 1:].apply(lambda x: (x-x.mean())/x.std(), axis=0)
+
+# 主成分分析の実行
+pca = PCA()
+pca.fit(dfs)
+
+# データを主成分空間に写像 & 都道府県のラベルを貼る
+feature = pd.DataFrame(pca.transform(dfs), index=df.values[:, 0])
+
+# 寄与率
+explained_variance_ratio = pd.DataFrame(pca.explained_variance_ratio_, index=[
+    "PC{}".format(x + 1) for x in range(len(dfs.columns))])
+
+print(explained_variance_ratio)
+
+# PCA の固有ベクトル
+eigenvector = pd.DataFrame(pca.components_, columns=df.columns[1:], index=[
+    "PC{}".format(x + 1) for x in range(len(dfs.columns))])
+
+print(eigenvector)
