c87e4d699b56a1892ec64c2a23488653c920cacf
nakstsu <koki20001206@outllok.jp>
2023-12-10 16:29:26 +0000

    add 主成分分析/plot

--- kadai/1/kadaikai.py
+++ kadai/1/kadaikai.py
@@ -2,6 +2,22 @@ from math import sqrt
 import pandas as pd
 from sklearn import linear_model
 from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
+
+import os
+import sys
+
+# Get the current script's directory
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Get the parent directory of the script's directory
+parent_dir = os.path.dirname(script_dir)
+
+# Add the parent directory to the Python path
+sys.path.append(parent_dir)
+
+from utils import convert_prefecture_to_english  # noqa
+
 
 economicData = pd.read_csv(
     '1/県内総生産.csv', sep=',')
@@ -46,7 +62,7 @@ print("自由度調整済み決定係数:", adjusted_r_squared)
 
 
 # 主成分分析
-
+0.9975577018737847
 all_merged_data = pd.merge(economicData, merged_data, on='都道府県')
 df = all_merged_data[['都道府県', '名目県内総生産', '市', '町',
                       '村', '総面積', '0歳から14歳', '15歳から64歳', '65歳以上']]
@@ -54,20 +70,25 @@ df = all_merged_data[['都道府県', '名目県内総生産', '市', '町',
 dfs = df.iloc[:, 1:].apply(lambda x: (x-x.mean())/x.std(), axis=0)
 
 # 主成分分析の実行
-pca = PCA()
+# 主成分は二つ
+n_components = 2
+pca = PCA(n_components=n_components)
 pca.fit(dfs)
 
-# データを主成分空間に写像 & 都道府県のラベルを貼る
-feature = pd.DataFrame(pca.transform(dfs), index=df.values[:, 0])
+# データを主成分空間に写像
+feature = pd.DataFrame(pca.transform(dfs), columns=[
+                       "PC1", "PC2"])
+ax = feature.plot(kind="scatter", x="PC1", y="PC2")
+for i, label in enumerate(df["都道府県"]):
+    ax.annotate(convert_prefecture_to_english(label),
+                (feature.iloc[i, 0], feature.iloc[i, 1]))
+plt.show()
 
-# 寄与率
-explained_variance_ratio = pd.DataFrame(pca.explained_variance_ratio_, index=[
-    "PC{}".format(x + 1) for x in range(len(dfs.columns))])
 
-print(explained_variance_ratio)
-
-# PCA の固有ベクトル
+# PCA の寄与率と固有ベクトル
+explained_variance_ratio = pd.DataFrame(pca.explained_variance_ratio_, columns=[
+    "寄与率"], index=["PC{}".format(x + 1) for x in range(n_components)])
 eigenvector = pd.DataFrame(pca.components_, columns=df.columns[1:], index=[
-    "PC{}".format(x + 1) for x in range(len(dfs.columns))])
-
+    "PC{}".format(x + 1) for x in range(n_components)])
+print(explained_variance_ratio)
 print(eigenvector)
--- kadai/utils/__init__.py
+++ kadai/utils/__init__.py
@@ -2,3 +2,7 @@ from utils.csv_to_models import (
     CSVMixin,
     parse_csv_file_to_models
 )
+
+from utils.prefecture_to_english import (
+    convert_prefecture_to_english
+)
